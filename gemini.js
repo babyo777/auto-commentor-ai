8b0250f91f9e2a6e8fe578d4d18862c2a71e431b
babyo77 <onlybabyyo7@gmail.com>
2024-05-30 14:13:19 +0530

    new

---
+++ gemini.js
@@ -0,0 +1,40 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const API_KEY = "AIzaSyCWFXGhEaPfOlRPcGkNJQPb0hD7dM61jks";
+
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+async function generateComments(text, path) {
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const chat = model.startChat({
+      history: [],
+      generationConfig: {
+        maxOutputTokens: Infinity,
+      },
+    });
+
+    const prompt = `
+   You're a Code commentor, your job is to only write comments when provided code and output only the modified raw code with comments without using markdown. You MUST NOT modify the code in any way.
+     Write comments in the following file ${path} with code content -
+ 
+     ${text}
+ 
+     If the code doesn't make sense or the content of code doesn't seem like code send response 'NONE' only.
+ 
+ 
+   `;
+
+    const result = await chat.sendMessage(prompt);
+    const res = await result.response;
+    const output = await res.text();
+    if (output.length > 0) {
+      return output;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    return null;
+  }
+}
+
+export default generateComments;
---
+++ index.js
@@ -0,0 +1,107 @@
+#!usr/bin/env node
+import fs from "fs";
+import path from "path";
+import * as cli from "@clack/prompts";
+import ora from "ora";
+import generateComments from "./gemini.js";
+
+const destDir = path.resolve("./Generated/");
+const excludeFilePath = path.resolve("./.exclude");
+
+if (!fs.existsSync(excludeFilePath)) {
+  fs.writeFileSync(excludeFilePath, "");
+}
+
+let excludedPaths = fs
+  .readFileSync(excludeFilePath, "utf8")
+  .split("\n")
+  .map((entry) => entry.trim())
+  .filter((entry) => entry !== "");
+
+excludedPaths = [
+  ...["Generated", ".exclude", "node_modules"],
+  ...excludedPaths,
+];
+
+const ensureDirExists = async (dirPath) => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+};
+
+const copyFilesRecursive = async (source, destination, spinner) => {
+  const entries = fs.readdirSync(source, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const sourcePath = path.join(source, entry.name);
+    const destPath = path.join(destination, entry.name);
+
+    if (excludedPaths.includes(entry.name)) {
+      continue;
+    }
+
+    if (fs.existsSync(destPath)) {
+      continue;
+    }
+
+    if (entry.isDirectory()) {
+      ensureDirExists(destPath);
+      await copyFilesRecursive(sourcePath, destPath, spinner);
+    } else {
+      const fileExtension = path.extname(entry.name).toLowerCase();
+      if (
+        [
+          ".jpg",
+          ".jpeg",
+          ".png",
+          ".gif",
+          ".bmp",
+          ".webp",
+          ".svg",
+          ".json",
+          ".git",
+        ].includes(fileExtension) ||
+        [".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv", ".webm"].includes(
+          fileExtension
+        )
+      ) {
+        fs.copyFileSync(sourcePath, destPath);
+      } else {
+        const content = fs.readFileSync(sourcePath, "utf8");
+        if (content.length <= 7000) {
+          spinner.text = `commenting ${sourcePath}`;
+
+          const commentedContent = await generateComments(content, entry.name);
+          if (commentedContent !== null) {
+            fs.writeFileSync(destPath, commentedContent);
+          } else {
+            fs.copyFileSync(sourcePath, destPath);
+          }
+        } else {
+          fs.copyFileSync(sourcePath, destPath);
+        }
+      }
+    }
+  }
+};
+
+const sleep = (time = 1000) =>
+  new Promise((resolve) => setTimeout(resolve, time));
+
+async function main() {
+  cli.intro("Let's Comment your Code");
+  await sleep();
+
+  const sourceDir = await cli.text({
+    message: "Enter path of your source directory",
+  });
+
+  const spinner = ora("Scanning directory").start();
+  await ensureDirExists(destDir);
+  spinner.text = "Commenting...";
+  await copyFilesRecursive(sourceDir, destDir, spinner);
+  spinner.text = "Commented";
+  spinner.stop();
+}
+
+main();
